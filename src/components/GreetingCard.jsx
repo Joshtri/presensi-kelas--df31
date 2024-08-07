f51e32e76f0118d4069c081ec336dd37c7b8dd97
Joshtri <stuffofyos1516@gmail.com>
2024-08-07 14:38:51 +0800

    fix greeting card

--- src/components/GreetingCard.jsx
+++ src/components/GreetingCard.jsx
@@ -3,12 +3,14 @@
 import { Card } from "react-bootstrap";
 
 const GreetingCard = () => {
+
+  const live_ke = 4;
   return (
     <>
       <Card.Title>Hallo sahabat semua!</Card.Title>
       <Card.Text>
         Terima kasih sudah berpartisipasi dalam Pelatihan dengan Tema Database
-        (Database Design & Programming with SQL) Live Session-3. Tetap semangat
+        (Database Design & Programming with SQL) Live Session-{live_ke}. Tetap semangat
         untuk pelatihan-pelatihan selanjutnya.
       </Card.Text>
     </>
