f868f39dd7e1b0beaa7e126ff43ca0ef7131a8de
Joshtri <stuffofyos1516@gmail.com>
2024-08-13 12:19:21 +0800

    update: greeting card, adding static qoute wkwk

--- src/components/GreetingCard.jsx
+++ src/components/GreetingCard.jsx
@@ -1,5 +1,4 @@
-// GreetingCard.js
-// import React from "react";
+import React from "react";
 import { Card } from "react-bootstrap";
 import axios from 'axios';
 import { useEffect, useState } from "react";
@@ -7,10 +6,9 @@ import { useEffect, useState } from "react";
 const GreetingCard = () => {
   const [liveKe, setLiveKe] = useState(null);
 
-
-  const getSesiActive = async()=>{
+  const getSesiActive = async () => {
     try {
-      const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/set-sesi/latest`)
+      const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/set-sesi/latest`);
 
       if (response.data) {
         setLiveKe(response.data.sesiActive);
@@ -19,13 +17,29 @@ const GreetingCard = () => {
     } catch (error) {
       console.log("Failed to fetch sesi:", error);
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getSesiActive();
-  },[]);
+  }, []);
 
-  // const live_ke = 8;
+  const quoteStyle = {
+    fontStyle: "",
+    color: "#555",
+    padding: "10px",
+    borderLeft: "4px solid #007bff",
+    backgroundColor: "#f9f9f9",
+    margin: "20px 0",
+    fontSize: "0.88em",
+  };
+
+  const authorStyle = {
+    textAlign: "right",
+    fontWeight: "bold",
+    marginTop: "10px",
+    color: "#333",
+  };
+
   return (
     <>
       <Card.Title>Hallo sahabat semua!</Card.Title>
@@ -34,9 +48,18 @@ const GreetingCard = () => {
         (Database Design & Programming with SQL) Live Session-{liveKe || "loading"}. Tetap semangat
         untuk pelatihan-pelatihan selanjutnya.
       </Card.Text>
-      
+
+      <Card.Text style={quoteStyle}>
+        24 jam sehari adalah waktu yang panjang.<br />
+        Banyak hal yang bisa dikerjakan dalam sehari.<br />
+        Jangan membuang waktumu untuk hal yang tidak berguna.<br />
+        Waktu adalah sumber daya yang lebih berharga dari pada uang.<br />
+        Terus membangun, Candi Prambanan saja hanya di buat dalam semalam!
+        <div style={authorStyle}>- Kalimasada</div>
+      </Card.Text>
+
       <Card.Text>
-        <p  className="fw-bold">
+        <p className="fw-bold">
           *Pastikan Anda sudah check out :)
         </p>
       </Card.Text>
