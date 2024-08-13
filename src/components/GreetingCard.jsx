568eb309101e9022e8587487e6407670778a9eab
Joshtri <stuffofyos1516@gmail.com>
2024-08-13 11:59:04 +0800

    update: change static value into dynamic value in sesi_ke at greetingCard

--- src/components/GreetingCard.jsx
+++ src/components/GreetingCard.jsx
@@ -1,16 +1,37 @@
 // GreetingCard.js
 // import React from "react";
 import { Card } from "react-bootstrap";
+import axios from 'axios';
+import { useEffect, useState } from "react";
 
 const GreetingCard = () => {
+  const [liveKe, setLiveKe] = useState(null);
 
-  const live_ke = 8;
+
+  const getSesiActive = async()=>{
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/set-sesi/latest`)
+
+      if (response.data) {
+        setLiveKe(response.data.sesiActive);
+      }
+      console.log(response.data);
+    } catch (error) {
+      console.log("Failed to fetch sesi:", error);
+    }
+  }
+
+  useEffect(()=>{
+    getSesiActive();
+  },[]);
+
+  // const live_ke = 8;
   return (
     <>
       <Card.Title>Hallo sahabat semua!</Card.Title>
       <Card.Text>
         Terima kasih sudah berpartisipasi dalam Pelatihan dengan Tema Database
-        (Database Design & Programming with SQL) Live Session-{live_ke}. Tetap semangat
+        (Database Design & Programming with SQL) Live Session-{liveKe || "loading"}. Tetap semangat
         untuk pelatihan-pelatihan selanjutnya.
       </Card.Text>
       
